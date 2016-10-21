6a374fb6dffb43a8d2a4ae90b6f0e2b7de486bdf
TamaWilson <wilson_tama@hotmail.com>
2016-10-21 18:29:16 -0300

    Ajustes para apresentação em sala
    Ajustes na navegação, funções de inserir e relacionar nós.

--- paths/urls.py
+++ paths/urls.py
@@ -7,4 +7,7 @@ app_name = 'voyage'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^results/$', views.results, name='results'),
+    url(r'^insert/$', views.insert, name='insert'),
+    url(r'^conectar/$', views.conectar, name='conectar'),
+
 ]
--- paths/views.py
+++ paths/views.py
@@ -11,7 +11,7 @@ def index(request):
     localidades = []
     for node in Localidade.nodes:
         localidades.append(node.nome)
-    context = { "localidades" : localidades, "teste" : 1 }
+    context = { "localidades" : localidades, }
 
     return render(request, 'paths/index.html', context)
 
@@ -19,50 +19,83 @@ def results(request):
     origem = request.POST['origem']
     destino = request.POST['destino']
 
+    #Verifica no lado do servidor se a origem/destino são iguais ou escolhidos corretamente
     if(origem == destino) or (origem == "NOK") or (destino == "NOK"):
         context = { 'erro': True }
     else:
-        hora_local = datetime.datetime.now().time()
-        velocidade= "v_normal"
-    
+        hora_local = datetime.datetime.now().time() #carrega o horário local para uma variável
+        velocidade= "v_normal" #inicia a execução com a velocidade definida como normal
+
+        #Verifica se a variavel hora_local está no intervalo dos horários de pico    
         if (datetime.time(18,0,0,0) < hora_local and datetime.time(19,0,0,0) > hora_local) or (datetime.time(11,30,0,0) < hora_local and datetime.time(14,0,0,0) > hora_local): 
-            velocidade= "v_pico"
+            velocidade= "v_pico" #Se a condição for positiva a velocidade é definida para o horário de pico
     
         query = '''MATCH (start:Localidade {nome: '%s'}), (end:Localidade {nome: '%s'})
         MATCH p=(start)-[:CONECTA_COM*]->(end)
         WITH p,reduce(s = 0, r IN rels(p) | s + (60*r.distancia)/r.%s) AS dist
-        RETURN p, dist ORDER BY dist Limit 1''' % (origem, destino,velocidade)
+        RETURN p, dist ORDER BY dist Limit 1''' % (origem, destino,velocidade) #carrega a query com algoritmo de Dijkstra. Nesse momento o calculo de distância é realizado na proria query
         
         
-        results, meta = db.cypher_query(query) #RECORDLIST
-        if(len(results) <1):
+        results, meta = db.cypher_query(query) #executa a query retorna um record list contendo apenas 1 resultado (o menor)
+        if(len(results) <1): #caso nenhum resultado tenha retornado as flags de erro são definidas
             context = { "null_result": True, "erro": False }
         else:
-            result = results[0] #PEGANDO O UNICO INDEX DA RECORDLIST TEMOS O RECORD
-            raw_nodes = result['p'] #RECUPERANDO O PATH GRAPH COM OS NOS EM SI
+            result = results[0] #Recupera o unico indice do RecordList como um objeto do tipo Record
+            raw_nodes = result['p'] #Atraves do index 'p' retornamos o PathGraph com todos os nós
 
-            pre_nodes = [Localidade.inflate(row) for row in raw_nodes.nodes]
+            pre_nodes = [Localidade.inflate(row) for row in raw_nodes.nodes] #Inicializamos os nós para acessar suas propriedades
     
             nodes = []
-            for node in pre_nodes:
+            for node in pre_nodes: #percorremos a lista de nós para obter o nome deles em uma outra lista
                 node.refresh()
                 nodes.append(node.nome)
     
-            context = { 'caminho' : nodes, 'custo': round(result['dist'],2) , "erro": False, "null_result": False }
+            context = { 'caminho' : nodes, 'custo': round(result['dist'],2) , "erro": False, "null_result": False } #definimos o contexto com o nome dos nos, o custo total e as flags de controle da view
 
     return render(request, 'paths/results.html', context)
 
 
-def cadastrar(request):
+def insert(request): #funcao para inserir um no no banco de dados
+    
+    context = { 'insert': False, 'status':"Ocorreu um erro" }
 
-    nome = request.POST['nome']
-    v_normal = request.POST['v_normal']
-    v_pico = request.POST['v_pico']
-    distancia = request.POST['distancia']
+    if request.POST.get("nome"):
+        
+        nome_node = request.POST['nome']   #recupera o texto inserido na pagina
+        node = Localidade(nome=nome_node)  #cria a instancia de um objeto Localidade
+        node.save()    #salva o no no banco de dados   
+        context = { 'insert': True, 'status':"Localidade cadastrada com sucesso"}
+    return render(request, 'paths/insert.html', context)
+
 
     
+def conectar(request): #funcao para ligar um nó ao outro
+
+    #recupera as localidades e cria o contexto inicial da página de relacionamentos
+    localidades = [] 
+    for node in Localidade.nodes:
+        localidades.append(node.nome)
+    context = { "localidades" : localidades, 'insert':False, 'status': "<span style='color: red;font-weight: bolder;'>ERRO<span>"  }
     
+    if request.POST.get("distancia"):
+        origem = request.POST['origem']
+        destino = request.POST['destino']
+        vn = int(request.POST['v_normal'])
+        vp = int(request.POST['v_pico'])
+        distancia_n = float(request.POST['distancia'])
+
+        if(origem == destino) or (origem == "NOK") or (destino == "NOK"):
+             context = { "localidades" : localidades, 'insert': True, 'status': "<span style='color: red;font-weight: bolder;'>ERRO<span>" }
+        else:
+            #Busca no banco pelos nós escolhidos na pagina
+            node_origem = Localidade.nodes.get(nome=origem)
+            node_destino = Localidade.nodes.get(nome=destino)
+
+            #instancia um relacionamento entre os nós com os dados informados
+            relation = node_origem.conectar.connect(node_destino, {'distancia':distancia_n, 'v_normal':vn, 'v_pico':vp})
+            relation.save()     #salva o relacionamento no banco e apos isso configura o contexto da página de resposta.
+            context = { "localidades" : localidades, 'insert': True, 'status': "<span style='color: green;font-weight: bolder;'>CONECTADO COM SUCESSO<span>" }
 
 
+    return render(request, 'paths/conectar.html', context)
 
-    
