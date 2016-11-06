aebcfac0a98f43dc938d5d46a8d51a6f44deb9ff
TamaWilson <wilson_tama@hotmail.com>
2016-11-06 11:30:51 -0300

    Adding PyGraphviz support and Danger property
    For a better visualization now Voyage uses pygrahpviz to render the
    path. A nre property was added to show the danger level on the path.

--- paths/models.py
+++ paths/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 os.environ['NEO4J_REST_URL'] = 'http://neo4j:neo4@localhost:7474/db/data/'
 
@@ -19,25 +18,3 @@ class Localidade(StructuredNode):
     
     conectar = Relationship('Localidade', 'CONECTA_COM', model=ConectaRel) #Um atributo para representar um relacionamento possível para esse tipo de nó é criado baseado no modelo do ConectaRel
 
-=======
-import os
-os.environ['NEO4J_REST_URL'] = 'http://neo4j:neo4@localhost:7474/db/data/'
-
-from django.db import models
-
-from neomodel import (StructuredNode, StructuredRel, StringProperty, IntegerProperty,Relationship,FloatProperty)
-
-class ConectaRel(StructuredRel):
-    #cada atributo da classe está relacionado com um tipo de propriedade, os relacionamento definido por essa classe usam Float, além disso definimos que as propiedades são obrigatórias 
-    distancia = FloatProperty(required=True)
-    vnormal = FloatProperty(required=True)
-    vrush = FloatProperty(required=True)
-    danger = FloatProperty(required = True)
-    
-class Localidade(StructuredNode):
-     #Da mesma forma os relacionamentos são definidos no model, também definimos os nós e suas propriedades (além dos seus tipos) 
-    nome = StringProperty(unique_index=True, required=True)
-    
-    conectar = Relationship('Localidade', 'CONECTA_COM', model=ConectaRel) #Um atributo para representar um relacionamento possível para esse tipo de nó é criado baseado no modelo do ConectaRel
-
->>>>>>> origin/voyage_pygraphviz
